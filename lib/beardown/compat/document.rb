4d23a7b4e84429fc96cf5dba883a5c9b349bed25
wuusn <wuusin@gmail.com>
2018-03-08 18:50:00 +0800

    fix: forget the link

--- lib/beardown/compat/document.rb
+++ lib/beardown/compat/document.rb
@@ -6,7 +6,7 @@ module Beardown
       @blocks = [:head, :line_separator, :list_todo,
                  :list_unordered, :list_ordered, :quote, :codeblock,
                  :asset, :blankline, :p]
-      @spans  = [:codespan, :hashtag_full, :hashtag_full_nospace, :hashtag,
+      @spans  = [:codespan, :hashtag_full, :hashtag_full_nospace, :hashtag, :linkpost, :linkurl,
                  :bold, :italic, :strike, :underline, :mark_nospace, :mark]
 
       @extensions = Array.new
--- lib/beardown/compat/version.rb
+++ lib/beardown/compat/version.rb
@@ -1,5 +1,5 @@
 module Beardown
   module Compat
-    VERSION = "0.1.0"
+    VERSION = "0.1.2"
   end
 end
